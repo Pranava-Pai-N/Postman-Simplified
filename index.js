c449ceda3f4abb5e33da8ce7c9b9a1c14f6ec584
Pranava Pai N <pranavpai.sahyadri@gmail.com>
2026-05-02 01:55:39 +0530

    Feat: Initial  Commit

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 export { requestInterceptor } from "./requestInterceptor.js";
-export { convertToPostman } from "./postman-mapper.js"; // if you have this file
+export { convertToPostman } from "./postman-mapper.js"; 
